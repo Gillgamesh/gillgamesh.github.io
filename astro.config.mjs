199be00aea345168f9318861bdc47e0b3aa9ace6
Gilvir Gill <gilvirgillgg@gmail.com>
2025-08-16 22:30:03 -0400

    cleaned up extra stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,23 @@ import tailwind from "@astrojs/tailwind";
 import { template } from "./src/settings";
 
 import sitemap from "@astrojs/sitemap";
+import rehypeCitation from 'rehype-citation';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), tailwind(), sitemap()],
-    site: template.website_url,
-    base: template.base,
+  integrations: [react(), tailwind(), sitemap()],
+  site: template.website_url,
+  base: template.base,
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeCitation,
+        {
+          bibliography: "./public/references.bib",
+          csl: "./public/acm-sig-proceedings.csl",
+          linkCitations: true,
+        },
+      ],
+    ],
+  },
 });
--- src/data/cv.tsx
+++ src/data/cv.tsx
@@ -19,14 +19,14 @@ export const education = [
 	{
 		school: 'Stony Brook University',
 		time: '2022 - 2027',
-		degree: 'Ph.D., Computer Science',
+		degree: 'Ph.D. in Computer Science',
 		location: 'Stony Brook, NY',
 		// description: 'Advised by Michael Bender and Joseph Mitchell',
 	},
 	{
 		school: 'Stony Brook University',
 		time: '2018 - 2022',
-		degree: 'B.S., Mathematics \& Computer Science',
+		degree: 'B.S in Mathematics \& Computer Science',
 		location: 'Stony Brook, NY',
 		// description: 'Advised by Michael Bender and Joseph Mitchell',
 	},
@@ -52,13 +52,22 @@ export const skills = [
 
 export const publications = [
 	{
-		title: '',
-		authors: '',
-		journal: '',
-		time: '',
-		link: '',
+		title: 'Exploring the Landscape of Distributed Graph Sketching',
+		authors: 'David Tench, Evan T West, Kenny Zhang, Michael A Bender, Daniel DeLayo, Martín Farach-Colton, Gilvir Gill, Tyler Seip, Victor Zhang',
+		journal: <>2025 Proceedings of the Symposium on Algorithm Engineering and Experiments (<strong>ALENEX</strong>)</>,
+		time: '2025',
+		link: 'https://epubs.siam.org/doi/abs/10.1137/1.9781611978339.11',
 		abstract: '',
+		sourceLink: 'https://doi.org/10.5281/zenodo.13845156'
 	},
+	{
+		title: 'Robust language-based mental health assessments in time and space through social media',
+		authors: 'Siddharth Mangalik, Johannes C Eichstaedt, Salvatore Giorgi, Jihu Mun, Farhan Ahmed, Gilvir Gill, Adithya V. Ganesan, Shashanka Subrahmanya, Nikita Soni, Sean AP Clouston, H Andrew Schwartz',
+		journal: <><strong>npj Digital Medicine</strong></>,
+		time: '2024',
+		link: 'https://www.nature.com/articles/s41746-024-01100-0',
+		abstract: '',
+	}
 	// {
 	// 	title: 'The Radiation of Uranium Compounds',
 	// 	authors: 'Marie Curie',
